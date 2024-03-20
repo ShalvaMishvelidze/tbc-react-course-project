3d1de484ec734ddbbe9f9fdb9299b024a736d97f
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-03-20 21:23:59 +0400

    added content and scss for contacts page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact">
+      <div className="contact-info">
+        <h3 className="contact-info-header">contact us</h3>
+        <p className="contact-info-number">(+123) 456 789 10</p>
+        <h5 className="contact-info-email">us@example.com</h5>
+      </div>
+      <div className="contact-form">
+        <h3 className="contact-form-header">contact form</h3>
+        <form>
+          <label>name</label>
+          <input type="text" />
+          <label>email</label>
+          <input type="email" />
+          <label>message</label>
+          <textarea></textarea>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
