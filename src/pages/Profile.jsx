ee2ef706111e93f3884ebb67071b354a955aa0e3
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-03-27 23:26:59 +0400

    minor refactoring to profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,17 @@
-import React from "react";
-
-// დაამატეთ შემდეგი გვერდები აპლიკაციაში,
-// სადაც მითითებული არაა ყველაფერი სტატიკური უნდა იყოს:
-// /profile
-// უნდა იყოს გამოტანილი ინფორმაცია მომხმარებელზე (სახელი, გვარი, მეილი...).
-// ორი password input: New Password & Confirm New Password.
-// ბოლოში შენახვის ღილაკი.
-
 const Profile = ({ name, lastName, email }) => {
   return (
     <form className="profile">
       <div>
         <label>name</label>
-        <input type="text" value={name} />
+        <input readOnly type="text" value={name} />
       </div>
       <div>
-        <label>lastname</label>
-        <input type="text" value={lastName} />
+        <label>last name</label>
+        <input readOnly type="text" value={lastName} />
       </div>
       <div>
         <label>email</label>
-        <input type="email" value={email} />
+        <input readOnly type="email" value={email} />
       </div>
       <div>
         <label>new password</label>
