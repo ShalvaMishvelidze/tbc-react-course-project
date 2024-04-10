a8b6f3eeb08c38ce361e4c6b1f5f47ad5669cf00
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-04-11 00:23:58 +0400

    made products page home page and started using api
    I deleted the old home page and made the products page the new home page. I stopped using the static data and switched over to using  api. I made dynamic route for displaying single product and implemented it.

---
+++ app/[id]/page.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+const { PRODUCTS_API_URL } = require("../../utils/constants");
+
+const SingleProduct = ({ params: { id } }) => {
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [image, setImage] = useState("");
+
+  const fetchProduct = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${PRODUCTS_API_URL}/${id}`);
+      const data = await response.json();
+      setProduct(data);
+      setImage(data.thumbnail);
+      setLoading(false);
+    } catch (e) {
+      setLoading(true);
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, [id]);
+
+  if (loading) {
+    return <div className="single-product">loading...</div>;
+  }
+
+  return (
+    <section className="single-product">
+      <h1 className="single-product-title">{product.title}</h1>
+      <div className="single-product-container">
+        <Image
+          src={image}
+          alt={product.title}
+          className="single-product-img"
+          width={420}
+          height={350}
+        />
+        <div className="single-product-img-container">
+          {product.images.map((img, index) => {
+            return (
+              <Image
+                key={index}
+                className={`single-product-img-container-small ${
+                  image === img && "active"
+                }`}
+                src={img}
+                alt={product.title}
+                width={100}
+                height={60}
+                onClick={() => setImage(img)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <p className="single-product-price">${product.price}</p>
+      <p className="single-product-desc">{product.description}</p>
+    </section>
+  );
+};
+export default SingleProduct;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,113 @@
-import React from "react";
+"use client";
+import { PRODUCTS_API_URL as api_url } from "../utils/constants";
+import Product from "../components/Product";
+import { Search } from "../components/Search";
+import { useEffect, useState } from "react";
+import Sort from "../components/Sort";
+import Link from "next/link";
 
-const Home = () => {
-  return <div>Home</div>;
+const Products = () => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [sortedProducts, setSortedProducts] = useState([]);
+  const [sort, setSort] = useState("a-z");
+  const [sortStatus, setSortStatus] = useState("cleared"); // cleared || updated || sorted
+  const [search, setSearch] = useState("");
+
+  const sortProducts = () => {
+    if (sortStatus === "sorted") {
+      setSortedProducts([...products]);
+      setSortStatus("cleared");
+      return;
+    }
+
+    setSortedProducts((_) => {
+      setSortStatus("sorted");
+      if (sort === "a-z") {
+        return [...products].sort((a, b) => a.title.localeCompare(b.title));
+      }
+      if (sort === "z-a") {
+        return [...products].sort((a, b) => b.title.localeCompare(a.title));
+      }
+      if (sort === "price-ascending") {
+        return [...products].sort((a, b) => a.price - b.price);
+      }
+      if (sort === "price-descending") {
+        return [...products].sort((a, b) => b.price - a.price);
+      }
+    });
+  };
+
+  const getProducts = async (searchString) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${api_url}/search?q=${searchString || search}`
+      );
+      const data = await response.json();
+      setProducts([...data.products]);
+      setLoading(false);
+    } catch (e) {
+      setLoading(true);
+      console.error(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSort((_) => {
+      setSortStatus("updated");
+      return e.target.value;
+    });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [search]);
+
+  useEffect(() => {
+    if (sortStatus === "cleared") {
+      setSortedProducts([...products]);
+      return;
+    }
+    sortProducts();
+  }, [products]);
+
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setSortStatus("updated");
+      setSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        getProducts(e.target.value);
+      }, 500);
+    };
+  };
+
+  return (
+    <section className="products">
+      <Search handleSearch={debounce()} search={search} />
+      <div className="products-header">
+        <h2>products</h2>
+        <Sort handleChange={handleChange} sortProducts={sortProducts} />
+      </div>
+      <div className="products-container">
+        {loading && (
+          <div className="loading">
+            <h1>loading...</h1>
+          </div>
+        )}
+        {loading ||
+          sortedProducts.map((product) => {
+            return (
+              <Link href={`/${product.id}`} key={product.id}>
+                <Product product={product} />
+              </Link>
+            );
+          })}
+      </div>
+    </section>
+  );
 };
 
-export default Home;
+export default Products;
