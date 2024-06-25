0d96e3ef7720f990355d1a1540df9292e2043079
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-06-25 10:45:38 +0400

    added meta tags for sharing to product

--- app/(dashboard)/store/[id]/page.tsx
+++ app/(dashboard)/store/[id]/page.tsx
@@ -7,6 +7,7 @@ import { Product } from "@/utils/interfaces";
 import Reviews from "@/components/Reviews";
 import Ratings from "@/components/Ratings";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import Head from "next/head";
 
 const SingleProduct = async ({
   params: { id },
@@ -25,21 +26,44 @@ const SingleProduct = async ({
     return <LoadingSpinner />;
   }
 
+  const pageTitle = `${product.name} - $${product.price}`;
+  const pageDescription = product.description;
+  const pageImage = product.image; // Ensure this is an absolute URL
+
   return (
-    <section className="single-product">
-      <ImageContainer product={product} />
-      <div className="single-product-container">
-        <h1 className="single-product-title">{product.name}</h1>
-        <p className="single-product-price">${product.price}</p>
-        <Ratings product_id={+id} rating={product.rating} />
-        <p className="single-product-desc">{product.description}</p>
-        <AddToCart
-          text={libraries[language].main.products.addToCart}
-          product={product}
-        />
-      </div>
-      <Reviews text={libraries[language].main.products} product_id={id} />
-    </section>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        {/* Open Graph / Facebook Meta Tags */}
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:image" content={pageImage} />
+        <meta property="og:type" content="product" />
+        {/* <meta property="og:url" content={url} /> */}
+
+        {/* X (Twitter) Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageDescription} />
+        <meta name="twitter:image" content={pageImage} />
+
+        {/* Add additional meta tags for other social media platforms as needed */}
+      </Head>
+      <section className="single-product">
+        <ImageContainer product={product} />
+        <div className="single-product-container">
+          <h1 className="single-product-title">{product.name}</h1>
+          <p className="single-product-price">${product.price}</p>
+          <Ratings product_id={+id} rating={product.rating} />
+          <p className="single-product-desc">{product.description}</p>
+          <AddToCart
+            text={libraries[language].main.products.addToCart}
+            product={product}
+          />
+        </div>
+        <Reviews text={libraries[language].main.products} product_id={id} />
+      </section>
+    </>
   );
 };
 export default SingleProduct;
