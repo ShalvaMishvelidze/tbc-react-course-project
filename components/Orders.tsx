087d148d9b0662c1eb7f171d95e5e475520e06f6
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-06-25 14:32:38 +0400

    toast fix

--- components/Orders.tsx
+++ components/Orders.tsx
@@ -37,29 +37,31 @@ const Orders = ({ language }: { language: string }) => {
   }
 
   return (
-    <section className="orders">
+    <>
       <Toast />
-      {orders.map((order) => {
-        const userOffsetMinutes = order.created_at.getTimezoneOffset();
+      <section className="orders">
+        {orders.map((order) => {
+          const userOffsetMinutes = order.created_at.getTimezoneOffset();
 
-        const userOffsetMillis = userOffsetMinutes * 60 * 1000;
-        const localDateObj = new Date(
-          order.created_at.getTime() - userOffsetMillis
-        );
+          const userOffsetMillis = userOffsetMinutes * 60 * 1000;
+          const localDateObj = new Date(
+            order.created_at.getTime() - userOffsetMillis
+          );
 
-        const formattedLocalDate = localDateObj.toLocaleString();
-        return (
-          <div className="order" key={order.order_id}>
-            <p>{order.title}</p>
-            <p>{order.price}$</p>
-            <p>{formattedLocalDate}</p>
-            <button type="button" onClick={() => handleClick(order.order_id)}>
-              {language === "en" ? "cancel" : "გაუქმება"}
-            </button>
-          </div>
-        );
-      })}
-    </section>
+          const formattedLocalDate = localDateObj.toLocaleString();
+          return (
+            <div className="order" key={order.order_id}>
+              <p>{order.title}</p>
+              <p>{order.price}$</p>
+              <p>{formattedLocalDate}</p>
+              <button type="button" onClick={() => handleClick(order.order_id)}>
+                {language === "en" ? "cancel" : "გაუქმება"}
+              </button>
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
 };
 export default Orders;
